ed1288ddb85b66bc58e6643bbcb74c44a9b56d52
Nairnbru007 <31794657+Nairnbru007@users.noreply.github.com>
2023-03-20 22:49:52 +0300

    123

--- astro/settings.py
+++ astro/settings.py
@@ -89,13 +89,22 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'u1958943_default',
+        'USER': 'u1958943_default',
+        'PASSWORD': 'P8j31jWhjTCh0UK8',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
